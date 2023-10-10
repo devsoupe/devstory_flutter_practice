fd4c81b6a694131dd05397070c1307dda4bfff52
devsoupe <luvyum4e@gmail.com>
2023-10-10 23:16:15 +0900

    직렬화 & 역직렬화 - 실습

---
+++ dart_practice/bin/custom_class/json.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+
+class Person {
+  final String name;
+  final int age;
+
+  const Person({
+    required this.name,
+    required this.age,
+  });
+
+  factory Person.fromJson(Map<String, dynamic> json) {
+    return Person(
+      name: json['name'],
+      age: json['age'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'age': age,
+    };
+  }
+}
+
+void main() {
+  /// 네트워크 응답 문자열
+  String jsonString = '{"name": "철수", "age": 10}';
+
+  /// JSON 포멧 String -> Map<String, dynamic>
+  Map<String, dynamic> jsonMap = jsonDecode(jsonString);
+  print(jsonMap);
+
+  /// Map<String, dynamic> -> Person
+  Person person = Person.fromJson(jsonMap);
+  print(person);
+
+  /// Person -> Map<String, dynamic>
+  Map<String, dynamic> personMap = person.toJson();
+  print(personMap);
+
+  /// Map<String, dynamic> -> JSON 포멧 String
+  String personString = jsonEncode(personMap);
+  print(personString);
+}
