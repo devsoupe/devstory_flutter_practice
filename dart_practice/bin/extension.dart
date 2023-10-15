204e866e68a02c99df59853d94d1b2eb1aa1c30c
devsoupe <luvyum4e@gmail.com>
2023-10-15 22:31:56 +0900

    extension

---
+++ dart_practice/bin/extension.dart
@@ -0,0 +1,13 @@
+class Machine {}
+
+extension MyMachineExt on Machine {
+  void printing() => print('Printing...');
+
+  void scanning() => print('Scanning...');
+}
+
+void main() {
+  final machine = Machine();
+  machine.printing();
+  machine.scanning();
+}
