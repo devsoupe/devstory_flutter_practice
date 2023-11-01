7a346e4b6467c8b86d1d37ed609a95885907f49d
devsoupe <luvyum4e@gmail.com>
2023-11-01 22:27:01 +0900

    Provider 실습

--- apple_store/lib/main.dart
+++ apple_store/lib/main.dart
@@ -17,7 +17,26 @@
 //   }
 // }
 
-import 'package:apple_store/2-inherited_widget/home_page.dart';
+// import 'package:apple_store/2-inherited_widget/home_page.dart';
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: HomePage(),
+//     );
+//   }
+// }
+
+import 'package:apple_store/3-provider/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
